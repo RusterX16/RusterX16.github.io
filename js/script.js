b3e6dca0585bfb8001a8ed3a29eea188a9247bbf
BuildTools <unconfigured@null.spigotmc.org>
2022-03-11 05:46:50 +0100

    Move to laptop

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-function onHover(field) {
-    field.style.boxShadow = "0 0 10px 5px rgba(51, 51, 51, 0.8)";
-    field.style.transitionDuration = ".5s";
-}
-
-function onBlur(field) {
-    field.style.boxShadow = "0 0 0 0 rgba(51, 51, 51, 0)";
-    field.style.transitionDuration = ".5";
-}
+$(document).ready(function() {
+    
+}) ;
